7a7e6752c76e29b200e33f8b8d01857fa8aec0b9
chwangmin <ckm7907@naver.com>
2022-12-02 20:07:47 +0900

    ✨Add Ec2StartView and Ec2StopView Function

--- condor/urls.py
+++ condor/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from condor import views
 
 urlpatterns = [
-    path('ec2list/', views.Ec2ListView.as_view())
+    path('ec2list/', views.Ec2ListView.as_view()),
+    path('ec2start/', views.Ec2StartView.as_view()),
+    path('ec2stop/', views.Ec2StopView.as_view())
 ]
--- condor/views.py
+++ condor/views.py
@@ -1,14 +1,108 @@
+from django.http import JsonResponse
+from rest_framework.response import Response
 from rest_framework.views import APIView
 import boto3
-
-from rest_framework.response import Response
 from django.conf import settings
+from botocore.exceptions import ClientError
+import sys
 
+ec2client = boto3.client('ec2', aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+                         aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
+
+
+# 1
 class Ec2ListView(APIView):
     def get(self, request):
-        ec2 = boto3.resource('ec2', aws_access_key_id=settings.AWS_ACCESS_KEY_ID, aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
+        data = {'ec2': []}
 
-        for instance in ec2.instances.all():
-            print(instance.id)
+        response = ec2client.describe_instances()
 
-        return Response({'good'}, status=200)  # 프론트로 전달
+        for reservation in response["Reservations"]:
+            for instance in reservation["Instances"]:
+                data['ec2'].append({
+                    "[id]": instance["InstanceId"],
+                    "[AMI]": instance["ImageId"],
+                    "[type]": instance["InstanceType"],
+                    "[state]": instance["State"]["Name"],
+                    "[monitoring state]": instance["Monitoring"]
+                })
+
+        return JsonResponse(data)
+
+
+# 2
+class Ec2AvailZXView(APIView):
+    def post(self, request):
+        return JsonResponse()
+
+
+# 3
+class Ec2StartView(APIView):
+    def post(self, request):
+        instance_id = request.data["instance_id"]
+        try:
+            ec2client.start_instances(InstanceIds=[instance_id], DryRun=True)
+        except ClientError as e:
+            if 'DryRunOperation' not in str(e):
+                raise
+
+            # Dry run succeeded, run start_instances without dryrun
+        try:
+            response = ec2client.start_instances(InstanceIds=[instance_id], DryRun=False)
+            print(response)
+        except ClientError as e:
+            print(e)
+        return Response({
+            'message': instance_id + ' start'
+        })
+
+
+# 4
+class Ec2AvailRView(APIView):
+    def get(self, request):
+        return JsonResponse()
+
+
+# 5
+class Ec2StopView(APIView):
+    def post(self, request):
+        instance_id = request.data["instance_id"]
+        try:
+            ec2client.stop_instances(InstanceIds=[instance_id], DryRun=True)
+        except ClientError as e:
+            if 'DryRunOperation' not in str(e):
+                raise
+
+            # Dry run succeeded, call stop_instances without dryrun
+        try:
+            response = ec2client.stop_instances(InstanceIds=[instance_id], DryRun=False)
+            print(response)
+        except ClientError as e:
+            print(e)
+        return Response({
+            'message': instance_id + ' stop'
+        })
+
+
+# 6
+class Ec2CreateView(APIView):
+    def get(self, request):
+        return JsonResponse()
+
+
+# 7
+class Ec2RebootView(APIView):
+    def get(self, request):
+        return JsonResponse()
+
+
+# 8
+class Ec2ListImageView(APIView):
+    def get(self, request):
+        return JsonResponse()
+
+
+# 99
+class Ec2QuitView(APIView):
+    def get(self, request):
+        return JsonResponse()
